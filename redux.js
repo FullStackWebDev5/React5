54ea51287d4000a19d2a4f9b61e65d9295cf8c1c
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-03-15 02:43:15 +0530

    Redux Intro

---
+++ redux.js
@@ -0,0 +1,37 @@
+const redux = require('redux')
+
+// Action Type Constant
+const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
+const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+
+// Action
+{
+  type: INCREMENT_COUNTER
+}
+
+{
+  type: DECREMENT_COUNTER
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Reducer
+// 1. Initial State
+// 2. Actions and corresponding updates 
