b6865f64ddb86439e5b4d24850b58c116d40a4c6
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-03-21 23:37:22 +0530

    React-Redux: Dispatch action with payload, default payload

--- src/components/Count/Count.js
+++ src/components/Count/Count.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Count.css'
-import { incrementCounter } from '../../redux'
+import { incrementCounter, decrementCounter } from '../../redux'
 import { connect } from 'react-redux';
 
 function Count(props) {
+  // const [countInput, setCountInput] = useState(0)
   return (
     <div style={{textAlign: 'center'}}>
       <h1>Count</h1>
       <h1 className="count-display">{props.count}</h1>
-      <button className="count-button" onClick={props.incrementCounter}>Increment</button>
+      {/* <input type="number" className="count-input" value={countInput} onChange={(e) => setCountInput(Number(e.target.value))} /> */}
+      <button className="count-button" onClick={() => props.incrementCounter()}>Increment</button>
+      <button className="count-button" onClick={() => props.decrementCounter()}>Decrement</button> <br />
+      <button className="count-button" onClick={() => props.incrementCounter(3)}>Increment 3</button>
+      <button className="count-button" onClick={() => props.decrementCounter(3)}>Decrement 3</button>
     </div>
   )
 }
@@ -21,7 +26,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    incrementCounter: () => dispatch(incrementCounter())
+    incrementCounter: (countInput) => dispatch(incrementCounter(countInput)),
+    decrementCounter: (countInput) => dispatch(decrementCounter(countInput))
   }
 }
 
--- src/redux/count/action.js
+++ src/redux/count/action.js
@@ -1,5 +1,10 @@
 export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
+export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
 
-export const incrementCounter = () => {
-  return { type: INCREMENT_COUNTER }
+export const incrementCounter = (countInput = 1) => {
+  return { type: INCREMENT_COUNTER, payload: countInput }
+}
+
+export const decrementCounter = (countInput = 1) => {
+  return { type: DECREMENT_COUNTER, payload: countInput }
 }
